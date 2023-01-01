4bd935cc40f7c40d189fc67aeeddcb100ac2d25c
Chris Steel <christopher.steel@helsing.ai>
2023-01-01 21:41:43 +0000

    latest

--- 2022/Day12/shortest_path.py
+++ 2022/Day12/shortest_path.py
@@ -6,3 +6,10 @@ class GraphNode:
     value: int
     distance_from_goal: int
     links: List["GraphNode"]
+
+def parse_input(file_name: str) -> List[List[GraphNode]]:
+    nodes = []
+    with open(file_name, "r") as rows:
+        for row in rows.readlines():
+            row = row.strip()
+            nodes.append([])
